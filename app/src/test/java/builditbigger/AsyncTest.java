37a46a1a00bec87be000ded8f20eafdbf52821c9
rodrigo <rodrigoaraujo990@gmail.com>
2018-03-12 18:39:07 -0300

    revisao

--- app/src/test/java/builditbigger/AsyncTest.java
+++ app/src/test/java/builditbigger/AsyncTest.java
@@ -1,26 +1,34 @@
 package builditbigger;
 
 import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.util.Pair;
 
 import com.example.myjokejava.MyJokeJava;
+import com.udacity.gradle.builditbigger.MainActivity;
+import com.udacity.gradle.builditbigger.task.GetJokeAsyncTask;
+
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Rodrigo on 11/03/2018.
  */
 
+@RunWith(AndroidJUnit4.class)
 public class AsyncTest {
 
-    private GetJokeAsyncTask mGetJokeAsyncTask = new GetJokeAsyncTask();
-    private MainActivity mMainActivity = new MainActivity();
+    public AsyncTest(){}
+
+    private Context mContext = InstrumentationRegistry.getTargetContext();
 
     @Test
     public void test() throws Throwable {
         try {
-            Assert.assertNotNull(new GetJokeAsyncTask().execute(new Pair<Context, String>(mMainActivity.getApplicationContext(), MyJokeJava.myJoke())));
+            Assert.assertNotNull(new GetJokeAsyncTask().execute(new Pair<Context, String>(mContext, MyJokeJava.myJoke())));
         } catch (Exception e) {
             Assert.fail("is null");
         }
